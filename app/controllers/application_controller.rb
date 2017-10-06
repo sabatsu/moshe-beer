7a029d5a5a8637da9cd66a482af099bf4cd9ee3b
sabatsu <sabatsuk@gmail.com>
2017-10-06 12:05:02 +0300

    sd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_s3
+
+  def set_s3
+  	@s3 = "https://s3.eu-central-1.amazonaws.com/moshe-beer"  	
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
 
-	def index
-		@s3 = "https://s3.eu-central-1.amazonaws.com/moshe-beer"
+	def index		
 	end
 	
 end
